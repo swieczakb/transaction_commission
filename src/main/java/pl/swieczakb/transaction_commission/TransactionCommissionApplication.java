89e9794ffec8331d1dcd8f87da4f988b59c53691
Bartlomiej Swieczak <bartlomiej.swieczak@tink.se>
2022-04-23 11:54:03 +0200

    feat(start): Apply initial files

---
+++ src/main/java/pl/swieczakb/transaction_commission/TransactionCommissionApplication.java
@@ -0,0 +1,13 @@
+package pl.swieczakb.transaction_commission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionCommissionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransactionCommissionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/swieczakb/transaction_commission/TransactionCommissionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.swieczakb.transaction_commission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCommissionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
