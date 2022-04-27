55b7ded1a9859b623efa737a1222c8b307458330
Bartlomiej Swieczak <bartlomiej.swieczak@tink.se>
2022-04-27 18:32:43 +0200

    rename(main): Change name of the main class

--- src/test/java/pl/swieczakb/transaction_commission/ApplicationTest.java
+++ src/test/java/pl/swieczakb/transaction_commission/ApplicationTest.java
@@ -3,8 +3,8 @@ package pl.swieczakb.transaction_commission;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TransactionCommissionApplicationTests {
+@SpringBootTest()
+class ApplicationTest {
 
   @Test
   void contextLoads() {
